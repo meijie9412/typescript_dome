aa35d3097576e201ee3bd8fb0440926de1d9d98c
meijie <656983975@qq.com>
2019-04-03 17:41:40 +0800

    基本类型

--- dome/type.ts
+++ dome/type.ts
@@ -8,3 +8,11 @@ let myName:string = 'meijie';
 let list:number[] = [1,2,3];
 
 let list2:Array<number> = [1,2,3];
+
+let x:[string,number];
+x = ['1',2];
+
+
+//枚举
+enum Color {red,green};
+let c: Color = Color.green;
