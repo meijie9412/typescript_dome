85623a03f79548842cea000e45ab224876fc5e8d
meijie <656983975@qq.com>
2019-04-09 17:40:10 +0800

    123

--- dome/interface2.ts
+++ dome/interface2.ts
@@ -49,3 +49,54 @@ mySearch = (source:string,substring:string) => {
     let result = source.search(substring);
    
 }
+
+//4 接口只读属性
+interface readonly_ife {
+    readonly name:string,
+    num:number,
+}
+
+const readonly_func = (obj:readonly_ife):void => {
+    obj.num = 1;
+    // obj.name = "meijie";
+}
+
+//可索引的类型
+interface myArrIfe {
+    [index:number]:string
+};
+
+let myArr:myArrIfe;
+myArr = ['123','321'];
+
+let myArrStr:string = myArr[0]
+
+//类 类型 实现接口
+
+interface classIfe {
+    name:string,
+    nowDate:Date,
+}
+
+class myClass implements classIfe {
+    name='1234';
+    nowDate:Date;
+    constructor(){
+
+    }
+}
+
+//接口继承
+
+interface oneIfe {
+    name:string
+}
+
+interface twoIfe extends oneIfe {
+    color:string
+}
+
+let squery :twoIfe ;
+squery as {};
+
+squery.name = '123';
